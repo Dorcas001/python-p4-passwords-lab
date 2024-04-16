a78debe5d79547bf131036c8ba11564da15617c3
karimi <karimidorcas1@gmail.com>
2024-04-16 16:32:58 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -19,23 +19,39 @@ class Signup(Resource):
     
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username']
-        )
+        user = User(username=json['username'])
         user.password_hash = json['password']
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+     def get(self):
+        if session.get('user_id'):
+            user=User.query.filter(User.id == session['user_id']).first()
 
+            return user.to_dict(), 200
+        return {}, 204
 class Login(Resource):
-    pass
+    def post(self):
+
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username)
+
+        password = request.get_json()['password']
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': 'Invalid username or password'}, 401
+
 
 class Logout(Resource):
-    pass
-
+     def delete(self):
+        session['user_id'] = None
+        return {}, 204
+        
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
